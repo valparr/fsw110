19f66e41c11b951320dfe82b71cf1e7637461b16
valparr <valencia.parr@bryanuniversity.edu>
2021-01-12 19:23:00 -0600

    creating element re-submit2

--- week2/creating&Appending_elements/creatingElements.js
+++ week2/creating&Appending_elements/creatingElements.js
@@ -5,29 +5,32 @@
 //Each li should also display text. Remember, you will need to first add the li’s to the ol before putting the ol on the DOM.
 
 //h1:
-var newH1 = document.createElement("h1")
-newH1.textContent = "Welcome to my JS site"
-var myheading = document.getElementById("heading")
-myheading.append(newH1)
+var h1 = document.createElement("h1")
+h1.innerHTML = "Welcome to my JS site"
+document.body.append(h1)
 
 //p:
-var newP = document.createElement("p")
-newP.textContent = "All of this was created with Javascript"
-var myText = document.getElementById("text")
-myheading.append(newP)
+var p = document.createElement("p")
+p.innerHTML = "All of this was created with Javascript"
+document.body.append(p)
 
 //ol with three li:
-var newLi = document.createElement("li")
-newLi.textContent = "Tacos"
-var myOrderLi = document.getElementById("favorite-foods")
-myOrderLi.append(newLi)
+var ol = document.createElement("ol")
+ol.innerHTML = "My Favorite Foods"
+document.body.append(ol)
 
-var newLi = document.createElement("li")
-newLi.textContent = "Shrimp Alfredo"
-var myOrderLi = document.getElementById("favorite-foods")
-myOrderLi.append(newLi)
+var li = document.createElement("li")
+li.innerHTML = "Tacos"
+ol.appendChild(li)
 
-var newLi = document.createElement("li")
-newLi.textContent = "Pan-Seared Salmon"
-var myOrderLi = document.getElementById("favorite-foods")
-myOrderLi.append(newLi)
+var li = document.createElement("li")
+li.innerHTML = "Shrimp Alfredo"
+ol.appendChild(li)
+
+var li = document.createElement("li")
+li.innerHTML = "Pan- Seared Salmon"
+ol.appendChild(li)
+
+var li = document.createElement("li")
+li.innerHTML = "Chicken n Waffles"
+ol.appendChild(li)
