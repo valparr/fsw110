c35a0d66f2b7091f2efdcc61197e6402f47cecdc
valparr <valencia.parr@bryanuniversity.edu>
2021-01-30 14:58:07 -0600

    calculator project

---
+++ week4/calculatorJS/calculatorProject.js
@@ -0,0 +1,75 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    const screenDisplay = document.getElementById('screen');
+    const spans = document.querySelectorAll('.buttons span');
+    const spanArray = Array.from(spans);
+    document.querySelectorAll('.operator')[1].innerText = '/'
+    document.querySelectorAll('.operator')[2].innertext = '*'
+    
+    spanArray.map(button => button.addEventListener('click', function(){
+        if (button.innerText !== "=") {
+            screenDisplay.innerText += button.innerText
+        };
+        if (button.innerText === "=") {
+            screenDisplay.innerText = eval
+            (screenDisplay.innerText)
+        };
+        if (button.innerText === "C") {
+            screenDisplay.innerText = ""
+        };
+    }));
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
